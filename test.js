3e73a18afafc4db2b329df5cad1ee0d0e51cd357
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-04-12 17:45:37 +0100

    Test original for underscore

--- test.js
+++ test.js
@@ -26,3 +26,15 @@ test('it returns undefined for tildes', (t, input) => {
     ['~~plain text here~~'],
     ['~~woop dee doo~~']
 ]);
+
+test('it returns an object with the correct \'original\' for one underscore', (t, input) => {
+
+    let output = emphasis(input);
+
+    t.assert.equal(input, output.original);
+
+}, [
+    ['_apples and oranges_'],
+    ['_plain text here_'],
+    ['_woop dee doo_']
+]);
