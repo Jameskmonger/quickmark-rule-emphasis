987081dc4726f52aa3b6adfac75b0aeb90881e67
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-04-12 17:44:48 +0100

    Test undefined for plain text

--- test.js
+++ test.js
@@ -2,3 +2,15 @@
 
 const test = require('modunit');
 const emphasis = require('./index');
+
+test('it returns undefined for plain text', (t, input) => {
+
+    let output = emphasis(input);
+
+    t.assert.equal(undefined, output);
+
+}, [
+    ['apples and oranges'],
+    ['plain text here'],
+    ['woop dee doo']
+]);
