5fd6b83ad6703e087cdfe7d5509f2c50b76adbaf
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-04-12 17:47:51 +0100

    Create regexps

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var createEmphasisRegExpTag = function (character) {
+    return '(?:\\' + character + ')';
+};
+
+var createEmphasisRegExp = function (tagCharacter) {
+    return new RegExp('^' + createEmphasisRegExpTag(tagCharacter)
+                        + '([\\s\\S]+?)'
+                        + createEmphasisRegExpTag(tagCharacter)
+                        + '$');
+};
+
+var UNDERSCORES_REGEXP = createEmphasisRegExp('_');
+var ASTERISKS_REGEXP = createEmphasisRegExp('*');
+
 module.exports = function (input) {
 
 };
