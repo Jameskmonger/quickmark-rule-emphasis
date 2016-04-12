fef10c6cd8cf959313cd5182ab09c03a55315e38
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-04-12 17:48:12 +0100

    Parse underscore

--- index.js
+++ index.js
@@ -15,5 +15,12 @@ var UNDERSCORES_REGEXP = createEmphasisRegExp('_');
 var ASTERISKS_REGEXP = createEmphasisRegExp('*');
 
 module.exports = function (input) {
+    var underscoreParsed = UNDERSCORES_REGEXP.exec(input);
 
+    if (underscoreParsed) {
+        return {
+            original: underscoreParsed[0],
+            parsed: underscoreParsed[1]
+        };
+    }
 };
